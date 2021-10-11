838274f2b03a4f5798a2018b03556c6b2bb8719d
semyung oh <semyung.dev@gmail.com>
2021-10-12 00:04:00 +0900

    Create Login, Signup component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from '../pages/Login';
+import Signup from '../pages/Signup';
 
 function App() {
-  return <h1>hi</h1>;
+  return (
+    <Switch>
+      <Route exact path="/login">
+        <Login />
+      </Route>
+      <Route exact path="/signup">
+        <Signup />
+      </Route>
+    </Switch>
+  );
 }
 
 export default App;
