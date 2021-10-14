89a7c4defbfe6914ab95043ba07e1c5a8978fef6
Wooseok Han <wswj9608@naver.com>
2021-10-15 05:18:23 +0900

    추가 - 업데이트 기능인데 아직 다 못했습니다

--- src/features/posts/actions.js
+++ src/features/posts/actions.js
@@ -12,7 +12,6 @@ import {
   REMOVE_COMMENT,
 } from './types';
 
-import axiosInstace from '../../api/axiosInstace';
 import T from '../../api/tokenInstance';
 
 // ActionCreator
@@ -104,19 +103,11 @@ export const createPostToAxios = (post) => async (dispatch) => {
   }
 };
 
-// 로컬에서 사용한 로직
 export const updatePostToAxios =
-  (postId, updateContents) => async (dispatch, getState) => {
-    const isPost = getState().posts.byId[postId];
-    // 바꿀 내용을 axios에 올리면 백에서 바뀐 원본데이터를 받아오는걸로 알고있는데
-    // 제가 구현한 방식은 업데이트가 완료 된 포스트를 그냥 DB에 올리는 것 같습니다.
-    const updateStatePost = { ...isPost, ...updateContents };
-    const editPost = await axiosInstace.editPost(postId, updateStatePost);
-    console.log(editPost.data);
-    // editPost안에 postId 들어있는데
-    // 굳이 리듀서에도 postId를 넘겨줘야 하나?
-    // 세명님이 보시고 알려주시겠죠?ㅎㅎ;
-    dispatch(updatePost(postId, editPost.data));
+  (postId, updateContents) => async (dispatch) => {
+    const res = await T.UPDATE('/post', postId, updateContents);
+    dispatch(updatePost(postId, res.data));
+    console.log(res.data);
   };
 
 // API 명세서 대로 전달 합니다.
